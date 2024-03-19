5478342eb16c90723c853c39cc6ed1d9f01bfa90
andriibilyanskii <andriibilyanskii@gmail.com>
2024-03-19 15:30:44 +0200

    deploy

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 const MONGO_URL = 'mongodb+srv://andriibilyanskii:tTDABgQCJQlQyyMV@chat.ws57ca8.mongodb.net/';
 
 async function runMongo() {
-	await mongoose.connect(MONGO_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	await mongoose.connect(MONGO_URL);
 }
 
 module.exports = runMongo;
